cc41f06e4fd10a764c40fdb2b6ce44083a7b812e
Vance Tran <v@vancetran.com>
2015-07-18 17:45:49 -0700

    Multiple routes

--- builds/dev/scripts/app.js
+++ builds/dev/scripts/app.js
@@ -6,5 +6,15 @@ dadjokes.config(['$routeProvider', function($routeProvider){
   $routeProvider.
     when('/login',{
       templateUrl: 'views/login.html'
-    })
+    }).
+    when('/register',{
+      templateUrl: 'views/register.html'
+    }).
+    when('/jokes',{
+      templateUrl: 'views/jokes.html'
+    }).
+    otherwise({
+      redirectTo: '/login'
+    });
+
 }]);
